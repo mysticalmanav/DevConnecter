502e8bfa5c799b12298f0f44e158653266793390
mysticalmanav <22je0539@iitism.ac.in>
2023-07-22 08:41:16 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use("/api/posts", require("./routes/api/posts"));
 //static assets in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
 const PORT = process.env.PORT || 5000;
