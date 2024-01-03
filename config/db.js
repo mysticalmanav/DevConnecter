2de51e31637fef3b17161f2031dc26f698842d79
mysticalmanav <22je0539@iitism.ac.in>
2024-01-03 14:47:58 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,19 +1,17 @@
-const mongoose  = require('mongoose');
-const config = require('config');
-const db= process.env.mongoURI;
+const mongoose = require("mongoose");
+const config = require("config");
+const db = process.env.mongoURI;
 
-const connectDB = async ()=>{
-    try{
-    
-     await mongoose.connect(db,{
-        useNewUrlParser:true,
-      
-     });
-     console.log('MongoDB Conected ...')
-    }catch(err){
-        console.log(err.message);
-        //exit process with failure
-        process.exit(1);
-    } 
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+    console.log("MongoDB Conected ...");
+  } catch (err) {
+    console.log(err.message);
+    //exit process with failure
+    process.exit(1);
+  }
+};
 module.exports = connectDB;
